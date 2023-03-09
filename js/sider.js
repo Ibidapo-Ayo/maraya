e0adf036d1f132aa12c39f6a9c52315bff9355e7
Ibidapo Ayomide Victor <ibidapoayomide754@gmail.com>
2023-03-09 20:31:02 +0100

    Testimonial Slider added

---
+++ js/sider.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".mobile_mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
